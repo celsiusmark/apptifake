805df69395a2602ffb6d0b46f87fdf6b0df77fba
CelsiusMark <celsius@apptivate.co>
2013-02-26 12:37:24 +0800

    Almost there 3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '545798818787879', '7f8c8424fbb578f3a066050cfb4adf8b',
+  provider :facebook, '284771504982089', 'b9aeed25aa6d94955d62754e1df954f0',
            {scope: "email,user_birthday,status_update,user_about_me,user_location,user_hometown,user_likes,publish_stream"}
 end
