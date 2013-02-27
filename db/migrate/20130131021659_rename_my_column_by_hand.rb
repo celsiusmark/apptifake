968c74d55bef444358d4f35c607cd1be0e2662e6
CelsiusMark <celsius@apptivate.co>
2013-02-27 16:56:48 +0800

    test

--- db/migrate/20130131021659_rename_my_column_by_hand.rb
+++
@@ -1,9 +0,0 @@
-class RenameMyColumnByHand < ActiveRecord::Migration
-  def up
-    rename_column :users, :fb_uid, :uid_fb
-  end
-
-  def down
-    rename_column :users, :uid_fb, :fb_uid
-  end
-end
--- db/migrate/20130227084558_rename_column_by_hand.rb
+++
@@ -1,8 +0,0 @@
-class RenameColumnByHand < ActiveRecord::Migration
-  def up
-    rename_column :users, :fb_uid, :uid_fb
-  end
-
-  def down
-  end
-end
